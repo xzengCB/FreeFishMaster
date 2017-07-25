dfc23c367a2ae1d6efdc8617c19178bf47b0c1df
xinhai <xinhai.zeng@careerbuilder.com>
2017-07-25 11:18:31 +0800

    Add default encoding

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('utf8')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FreeFishMaster.settings")
