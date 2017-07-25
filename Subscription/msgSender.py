7133a4a0465b0ca066d0f2d729e7e8dd56cff574
xinhai <xinhai.zeng@careerbuilder.com>
2017-07-25 11:02:05 +0800

    change the format of msg

--- Subscription/msgSender.py
+++ Subscription/msgSender.py
@@ -1,16 +1,17 @@
-# coding: utf-8
-
+# -*- coding: utf-8 -*-
 import requests
 import shortUrl
+from .models import Analysis, Subscription
 
 def sendMsg(analysisId):
+    sub = Analysis.objects.get(id=analysisId).subscriptionID
     endpoint = 'https://api.twilio.com/2010-04-01/Accounts/AC616c2a9691d0f5c3b5123d3a44a70302/Messages.json'
-    resultUrl = 'http://mchen.cbapac.net:3000/matches/{0}'.format(analysisId)
+    resultUrl = 'http://10.63.88.166:3000/matches/{0}'.format(analysisId)
 
     payload = {
     'To': '+8615800599746',
     'From': '+12566678498',
-    'Body': '【闲鱼Pro】已发现您订阅的闲鱼商品，点击{0}查看详情'.format(resultUrl)
+    'Body': '发自【闲鱼Pro】\n 物品: {1} \n 价格: {2} - {3} \n 点击 {0} 查看详情'.format(resultUrl, sub.keywords.encode('utf-8'), sub.priceLow, sub.priceHigh)
     }
 
     res = requests.post(endpoint, data=payload)
